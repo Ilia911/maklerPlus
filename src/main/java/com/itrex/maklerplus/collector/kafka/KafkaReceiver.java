84e05960b0bb32d80b39c66ea533740303587310
Ilya Eremkin <ilya.eremkin@itrexgroup.com>
2022-05-18 06:39:08 +0000

    Correct after review Kafka Listener.

--- src/main/java/com/itrex/maklerplus/collector/kafka/KafkaReceiver.java
+++ src/main/java/com/itrex/maklerplus/collector/kafka/KafkaReceiver.java
@@ -1,8 +1,19 @@
 package com.itrex.maklerplus.collector.kafka;
 
 import com.itrex.maklerplus.collector.exception.ServiceException;
+import com.itrex.maklerplus.collector.service.AdvertService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Component;
 
-public interface KafkaReceiver {
+@Component
+@RequiredArgsConstructor
+public class KafkaReceiver {
 
-  void receiveMessage(String messages) throws ServiceException;
+  private final AdvertService advertService;
+
+  @KafkaListener(topics = "${topic.telegram.receive}")
+  public void receiveMessagesFromTelegram(String messages) throws ServiceException {
+    advertService.processMessagesFromKafka(messages);
+  }
 }
--- src/test/java/com/itrex/maklerplus/collector/util/TelegramMessageParserUtilTest.java
+++ src/test/java/com/itrex/maklerplus/collector/util/TelegramMessageParserUtilTest.java
@@ -1,7 +1,7 @@
 package com.itrex.maklerplus.collector.util;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import com.itrex.maklerplus.collector.entity.AdvertMessage;
 import java.util.List;
